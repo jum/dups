62b6974014fc1f22aa2de2ca12cbad8873dec592
Jens-Uwe Mager <jum@anubis.han.de>
2012-01-16 17:57:23 -0500

    Use the file size as the first indication that we have candidades that might have equal content. Only if the size matches, run the expensive SHA1 checksum.

--- dups.go
+++ dups.go
@@ -38,7 +38,7 @@ func (p StringLenSorter) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
 
 func main() {
 	flag.Parse()
-	tree := make(map[string][]string)
+	tree := make(map[int64][]string)
 	err := filepath.Walk(*root, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			debug("walkFn path %v, err %v\n", path, err)
@@ -49,44 +49,56 @@ func main() {
 			return nil
 		}
 		debug("walkFn path %v\n", path)
-		f, err := os.Open(path)
-		if err != nil {
-			//panic(err.Error())
-			// continue if file can not be opened
-			fmt.Fprintf(os.Stderr, "%v: %v\n", path, err)
-			return nil
-		}
-		defer f.Close()
-		var reader *bufio.Reader
-		//reader, err = bufio.NewReaderSize(f, 4*1024*1024)
-		reader = bufio.NewReader(f)
-		sha1 := sha1.New()
-		_, err = io.Copy(sha1, reader)
-		if err != nil {
-			panic(err.Error())
-		}
-		hash := hex.EncodeToString(sha1.Sum(nil))
-		debug("path %v, hash %v\n", path, hash)
-		tree[hash] = append(tree[hash], path)
+		sz := info.Size()
+		tree[sz] = append(tree[sz], path)
 		return nil
 	})
 	if err != nil {
 		panic(err.Error())
 	}
 	debug("tree %#v\n", tree)
-	for hash, flist := range tree {
+	for sz, flist := range tree {
 		if len(flist) < 2 {
 			continue
 		}
-		sort.Sort(StringLenSorter(flist))
-		fmt.Printf("files with hash %v:\n%v\n", hash, flist)
-		for _, file := range flist[:len(flist)-1] {
-			fmt.Printf("Deleting dup %v\n", file)
-			if *delete {
-				debug("really del %v\n", file)
-				err := os.Remove(file)
-				if err != nil {
-					panic(err.Error())
+		debug("File with size %v:\n", sz)
+		// we have a list of files with the same size, possibly candiates with equal content.
+		hashtree := make(map[string][]string)
+		for _, path := range flist {
+			f, err := os.Open(path)
+			if err != nil {
+				//panic(err.Error())
+				// continue if file can not be opened
+				fmt.Fprintf(os.Stderr, "%v: %v\n", path, err)
+				continue
+			}
+			var reader *bufio.Reader
+			//reader, err = bufio.NewReaderSize(f, 4*1024*1024)
+			reader = bufio.NewReader(f)
+			sha1 := sha1.New()
+			_, err = io.Copy(sha1, reader)
+			if err != nil {
+				panic(err.Error())
+			}
+			f.Close()
+			hash := hex.EncodeToString(sha1.Sum(nil))
+			debug("path %v, hash %v\n", path, hash)
+			hashtree[hash] = append(hashtree[hash], path)
+		}
+		for hash, flist := range hashtree {
+			if len(flist) < 2 {
+				continue
+			}
+			sort.Sort(StringLenSorter(flist))
+			fmt.Printf("files with hash %v:\n%v\n", hash, flist)
+			for _, file := range flist[:len(flist)-1] {
+				fmt.Printf("Deleting dup %v\n", file)
+				if *delete {
+					debug("really del %v\n", file)
+					err := os.Remove(file)
+					if err != nil {
+						panic(err.Error())
+					}
 				}
 			}
 		}
